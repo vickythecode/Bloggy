4d56259a5d362589c4913f3f50d52e7825765e29
Vignesh Raman <114500150+vickythecode@users.noreply.github.com>
2024-10-06 14:29:12 +0530

    Update postRoutes.js
    small mistake

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -5,7 +5,7 @@ const uploadMiddleware = require('../middlewares/uploadMiddleware');
 
 router.post('/post', uploadMiddleware.single('file'), postController.createPost);
 router.put('/post', uploadMiddleware.single('file'), postController.updatePost);
-router.get('/post', postController.getPosts);
+router.get('/posts', postController.getPosts);
 router.get('/post/:id', postController.getPostById);
 router.delete('/post/:id', postController.deletePost);
 
