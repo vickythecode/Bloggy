cfcb34bed77fc45b95926c5db4c2038a3ea94c24
Vignesh Raman <114500150+vickythecode@users.noreply.github.com>
2024-10-06 15:21:05 +0530

    Update EditPost.jsx

--- frontend/src/components/EditPost.jsx
+++ frontend/src/components/EditPost.jsx
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('https://bloggy-97fr.onrender.com/post', {
+    const response = await fetch('https://bloggy-97fr.onrender.com/post-update', {
       method: 'PUT',
       body: data,
       credentials: 'include',
